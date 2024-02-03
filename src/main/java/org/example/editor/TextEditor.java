7303e6b2248732154f39c1393fb3e88a8ee57f76
EKA <winataeka@yahoo.com>
2024-02-03 16:00:10 +0700

    [Eka] Add type, remove, moveLeft, moveRight functionality in TextEditor + Test

---
+++ src/main/java/org/example/editor/TextEditor.java
@@ -0,0 +1,54 @@
+package org.example.editor;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
+public class TextEditor {
+
+  private int cursorIndex;
+  private final List<Character> characters;
+
+  public TextEditor() {
+    this.cursorIndex = 0;
+    this.characters = new LinkedList<>();
+  }
+
+  public void type(String input) {
+    ListIterator<Character> iterator = characters.listIterator(this.cursorIndex);
+
+    for (char c : input.toCharArray()) {
+      iterator.add(c);
+      cursorIndex++;
+    }
+  }
+
+  public void delete() {
+    ListIterator<Character> iterator = characters.listIterator(this.cursorIndex);
+
+    if (iterator.hasPrevious()) {
+      iterator.previous();
+      iterator.remove();
+      this.cursorIndex = Math.max(0, this.cursorIndex - 1);
+    }
+  }
+
+  public void moveLeft() {
+    this.cursorIndex = Math.max(0, this.cursorIndex - 1);
+  }
+
+  public void moveRight() {
+    if (this.cursorIndex == this.characters.size()) {
+      return;
+    }
+    this.cursorIndex += 1;
+  }
+
+  public String getCurrentText() {
+    return characters.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining(""));
+  }
+
+}
---
+++ src/test/java/org/example/editor/TextEditorTest.java
@@ -0,0 +1,89 @@
+package org.example.editor;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TextEditorTest {
+
+  private TextEditor textEditor;
+
+  @BeforeEach
+  public void initialize() {
+    this.textEditor = new TextEditor();
+  }
+
+  @Test
+  void type_shouldAppendStringAsChars_whenInputIsNonEmpty() {
+    String expectedText = "abc\ndef";
+
+    this.textEditor.type("abc");
+    this.textEditor.type("\ndef");
+    String result = textEditor.getCurrentText();
+
+    Assertions.assertEquals(result, expectedText);
+  }
+
+  @Test
+  void delete_shouldRemoveCurrentCharAtCursor_whenCursorIsNotZero() {
+    String initialText = "abcd";
+    String expectedText = "ab";
+    this.textEditor.type(initialText);
+
+    this.textEditor.delete();
+    this.textEditor.delete();
+    String result = this.textEditor.getCurrentText();
+
+    Assertions.assertEquals(result, expectedText);
+  }
+
+  @Test
+  void moveLeft_shouldMoveCursorLeftOneUnit_whenCurrentIndexIsNotZero() {
+    String initialText = "abcd";
+    this.textEditor.type(initialText);
+
+    this.textEditor.moveLeft();
+    this.textEditor.moveLeft();
+    this.textEditor.type("zz");
+    String result = this.textEditor.getCurrentText();
+
+    Assertions.assertEquals(result, "abzzcd");
+  }
+
+  @Test
+  void moveLeft_shouldKeepCursorPosition_whenCurrentIndexIsZero() {
+    this.textEditor.moveLeft();
+    this.textEditor.type("a");
+    String result = this.textEditor.getCurrentText();
+
+    Assertions.assertEquals(result, "a");
+  }
+
+  @Test
+  void moveRight_shouldMoveCursorRightOneUnit_whenCurrentIndexIsNotEqualsTextLength() {
+    String initialText = "abcd";
+    this.textEditor.type(initialText);
+    this.textEditor.moveLeft();
+    this.textEditor.moveLeft();
+
+    this.textEditor.moveRight();
+    this.textEditor.type("zz");
+    String result = this.textEditor.getCurrentText();
+
+    Assertions.assertEquals(result, "abczzd");
+  }
+
+  @Test
+  void moveRight_shouldNotMoveCursor_whenCurrentIndexIsEqualsToTextLengthSize() {
+    this.textEditor.type("zzz");
+    this.textEditor.moveRight();
+    this.textEditor.moveRight();
+    this.textEditor.moveRight();
+
+    this.textEditor.type("ab");
+    String result = this.textEditor.getCurrentText();
+
+    Assertions.assertEquals(result, "zzzab");
+  }
+
+}
